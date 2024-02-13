e0436bc289da52ff5fb1e98ca1cfb5b672bc9aec
RaymondZziwa <raymondzian@gmail.com>
2024-02-13 04:19:08 +0300

    Wrote tests for the guest view

--- test/guest.e2e-spec.ts
+++ test/guest.e2e-spec.ts
@@ -1,4 +1,6 @@
+import { articles, events, partners, successStories } from '@prisma/client';
 import { setup, teardown } from './test-setup';
+import * as pactum from 'pactum';
 
 describe('Guest profile E2E', () => {
   beforeAll(async () => {
@@ -9,5 +11,50 @@ describe('Guest profile E2E', () => {
     await teardown();
   });
 
-  describe('Guest profile tests', () => {});
+  describe('Guest profile tests', () => {
+    //test to retrieve all partners
+    it('retrieve all partners', () => {
+      return pactum
+        .spec()
+        .get('/talent/talent-profile')
+        .expectStatus(200)
+        .expectJson('partners', (partners: partners[]) => {
+          expect(Array.isArray(partners)).toBe(true);
+          expect(partners).toBeGreaterThan(0);
+        });
+    });
+    //test to retrieve all events
+    it('retrieve all events', () => {
+      return pactum
+        .spec()
+        .get('/talent/talent-profile')
+        .expectStatus(200)
+        .expectJson('events', (events: events[]) => {
+          expect(Array.isArray(events)).toBe(true);
+          expect(events).toBeGreaterThan(0);
+        });
+    });
+    //test to retrieve all articles
+    it('retrieve all articles', () => {
+      return pactum
+        .spec()
+        .get('/talent/talent-profile')
+        .expectStatus(200)
+        .expectJson('articles', (articles: articles[]) => {
+          expect(Array.isArray(articles)).toBe(true);
+          expect(articles).toBeGreaterThan(0);
+        });
+    });
+    //test to retrieve all success stories
+    it('retrieve all success stories', () => {
+      return pactum
+        .spec()
+        .get('/talent/talent-profile')
+        .expectStatus(200)
+        .expectJson('articles', (successStories: successStories[]) => {
+          expect(Array.isArray(successStories)).toBe(true);
+          expect(successStories).toBeGreaterThan(0);
+        });
+    });
+  });
 });
